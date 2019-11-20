f44ea0806bb97f16d0eb0118dc73ea4e97f05711
Willy Romão <willyr.goncalves@gmail.com>
2019-11-20 23:35:57 +0000

    add a transfer and push_right to portal

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,18 +1,31 @@
 defmodule Portal do
-  @moduledoc """
-  Documentation for Portal.
-  """
+  defstruct [:left, :right]
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Portal.hello()
-      :world
-
+  Starts tranfering `data` from `left` to `right`.
   """
-  def hello do
-    :world
+  def transfer(left, right, data) do
+    # First add all data to the portal on left
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    # Returns a portal struct we will use
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right in given `portal`.
+  """
+  def push_right(portal) do
+    # Check if we can pop data from left.
+    # if so, push the popped data to the right
+    # or do nothing
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+    # Return the portal itself
+    portal
   end
 end
